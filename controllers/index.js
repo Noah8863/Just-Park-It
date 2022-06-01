62e64d3c6b8b3dfba9d7a2e65bdceffc4780cda3
Noah8863 <stacknoah99@gmail.com>
2022-05-31 18:13:02 -0600

    added index.js and loginPage file

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+router.get("/", (req, res) => {
+  res.render("homePage");
+});
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+
+module.exports = router;

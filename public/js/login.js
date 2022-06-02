e4495f2df7ab699ea68df4a892f22b2ea0fc9023
Noah8863 <stacknoah99@gmail.com>
2022-06-02 17:34:37 -0600

    adding login page

--- public/js/login.js
+++ public/js/login.js
@@ -56,3 +56,26 @@ document
   .addEventListener("submit", signupFormHandler);
 
 
+  const registerContainer = document.getElementById("registerContainer");
+  const signupContainer = document.getElementById("signup-container");
+  const signupWording = document.getElementById("noMember")
+  const signInText = document.getElementById("signBackIn")
+  const test = document.getElementById("test")
+  
+  registerbtn.addEventListener("click", registerUser)
+  
+  function registerUser() {
+    moveback()
+    signupContainer.classList.toggle("moveside")
+    registerContainer.classList.add("registering")
+    signInText.classList.toggle('hide')
+    signupWording.classList.toggle('hide')
+  
+  }
+  
+  
+  function moveback() {
+    if (registerContainer.classList.contains("registering")) {
+      signupContainer.classList.toggle('moveback')
+    }
+  }

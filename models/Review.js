36124eb7921bf2e2c261f6de21bd1c77f41fcd60
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-06-02 19:53:55 -0600

    review/css/user model

--- models/Review.js
+++ models/Review.js
@@ -1,36 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Reviews extends Model {}
 
-Reviews.init (
-    {
-        id: {
-            type:  DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-              model: 'user',
-              key: 'id',
-            },
-          },
+Reviews.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'review',
-    }
+    modelName: "review",
+  }
 );
 
 module.exports = Reviews;

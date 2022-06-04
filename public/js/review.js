3d3b862cd7f131f5688c7c148e05b248ee706ce9
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-06-04 13:45:18 -0600

    working review route

--- public/js/review.js
+++ public/js/review.js
@@ -3,9 +3,23 @@
 const reviewHandler = async (event) => {
   event.preventDefault();
   
-  const review = document.querySelector("#text-area").value.trim();
+  const description = document.querySelector("#text-area").value.trim();
 
-  if (review) {
-    const response = await 
+  if (description) {
+    const response = await fetch("api/reviews", {
+      method: "POST",
+      body: JSON.stringify({description}),
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(response)
+    if (response.ok) {
+      document.location.replace("/")
+    } else {
+      alert(response.statusText);
+    }
+  } else {
+    console.log("something went wrong")
   }
-}
+};
+
+document.querySelector("#review-input").addEventListener("submit", reviewHandler)

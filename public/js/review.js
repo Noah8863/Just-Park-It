89de47856b549942f0c30c2322d7d9aa0adb698e
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-06-04 11:27:43 -0600

    dashboard handlebars/review.js

---
+++ public/js/review.js
@@ -0,0 +1,11 @@
+
+
+const reviewHandler = async (event) => {
+  event.preventDefault();
+  
+  const review = document.querySelector("#text-area").value.trim();
+
+  if (review) {
+    const response = await 
+  }
+}

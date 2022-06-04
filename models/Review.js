6c14afd964327f82404b63c4befe12e8ec089f31
darrinling <darrinling@gmail.com>
2022-06-04 11:29:24 -0600

    fixed login issues

--- models/Review.js
+++ models/Review.js
@@ -1,42 +1,39 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Reviews extends Model { }
+class Reviews extends Model {}
 
 Reviews.init(
-
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false,
-
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id',
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'review',
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "review",
+  },
 
   {
     id: {
@@ -69,7 +66,6 @@ Reviews.init(
     underscored: true,
     modelName: "review",
   }
-
 );
 
 module.exports = Reviews;

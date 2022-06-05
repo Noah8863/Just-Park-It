492ee610cf3a1174a3f01c7c02be4a9ba2dbe6d0
Noah8863 <stacknoah99@gmail.com>
2022-06-05 16:27:42 -0600

    fixing bugs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,7 +5,7 @@ const withAuth = require("../utils/auth");
 router.get("/", async (req, res) => {
   try {
     // Get all reviews and JOIN with user data
-    const reviewData = await Reviews.findAll({
+    const reviewData = await Review.findAll({
       include: [
         {
           model: User,
@@ -76,7 +76,7 @@ router.get("/login", (req, res) => {
     return;
   }
 
-  res.render("login");
+  res.render("loginPage");
 });
 
 module.exports = router;

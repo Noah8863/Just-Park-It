c59734b0d436ebb26590d5fdde99489955c4b512
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-06-05 16:26:32 -0600

    unload

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const reviewRoutes = require("./reviewRoutes");
+// const homeRoutes = require("./homeRoutes");
 
 router.use("/user", userRoutes);
 router.use("/reviews", reviewRoutes);
+// router.use("/", homeRoutes);
 
 module.exports = router;

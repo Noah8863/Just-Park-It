e4cbdd0de6b6ac9d80b2e8814b65f55e6e1ccdeb
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-06-05 18:56:50 -0600

    formatting/removed reviewhandlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const reviewRoutes = require("./reviewRoutes");
-// const homeRoutes = require("./homeRoutes");
 
 router.use("/user", userRoutes);
 router.use("/reviews", reviewRoutes);
-// router.use("/", homeRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,19 @@
 //Outer Index
 const router = require("express").Router();
-const Review = require("../models/Review")
+const Review = require("../models/Review");
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 router.get("/", async (req, res) => {
-  const result = await Review.findAll({})
-  const allReviews = result.map(review => {
+  const result = await Review.findAll({});
+  const allReviews = result.map((review) => {
     return review.get({
-      plain: true
-    })
-  })
-  console.log(allReviews)
+      plain: true,
+    });
+  });
+  console.log(allReviews);
   res.render("homePage", {
-    reviews: allReviews
+    reviews: allReviews,
   });
 });
 router.get("/login", (req, res) => {
@@ -25,8 +25,6 @@ router.get("/signup", (req, res) => {
 router.get("/dashboard", (req, res) => {
   res.render("dashboard");
 });
-router.get("/reviews", (req, res) => {
-  res.render("reviews");
-});
+
 
 module.exports = router;

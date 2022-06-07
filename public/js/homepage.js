52ac4a0357c8d2c681e1d0b6d65666d577b643a4
Noah8863 <stacknoah99@gmail.com>
2022-06-07 16:05:15 -0600

    added last minute styling and particles.js for new technology

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    window.setTimeout(function () {
-        document.body.classList.remove('fade');
-    }, 230);
+  window.setTimeout(function () {
+    document.body.classList.remove('fade');
+  }, 230);
 });
 document.body.classList.add('fade');
 
@@ -9,12 +9,119 @@ document.body.classList.add('fade');
 //Code snippet for the navbar 
 const list = document.querySelectorAll('.list');
 
-function activeLink (){
-        list.forEach((item) => 
-        item.classList.remove('active'));
-        this.classList.add('active');
+function activeLink() {
+  list.forEach((item) =>
+    item.classList.remove('active'));
+  this.classList.add('active');
+}
+list.forEach((item) =>
+  item.addEventListener("click", activeLink))
+
+
+//Code Snippet for the background
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 }
     }
-    list.forEach((item) => 
-    item.addEventListener("click", activeLink))
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "repulse" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
 
 
+
+// import {rating} from "/js/review.js"
+
+// const oneStarRatingFinal = document.getElementById('1starFinal')
+// const twoStarRatingFinal = document.getElementById('2starFinal')
+// const threeStarRatingFinal = document.getElementById('3starFinal')
+// const fourStarRatingFinal = document.getElementById('4starFinal')
+// const fiveStarRatingFinal = document.getElementById('5starFinal')
+
+
+// function final() {
+//       if (rating = 1) {
+//         oneStarRatingFinal.classList.add('checked')
+//         twoStarRatingFinal.classList.remove('checked')
+//         threeStarRatingFinal.classList.remove('checked')
+//         fourStarRatingFinal.classList.remove('checked')
+//         fiveStarRatingFinal.classList.remove('checked')
+//       }
+//       if (rating = 2) {
+//         oneStarRatingFinal.classList.add('checked')
+//         twoStarRatingFinal.classList.add('checked')
+//         threeStarRatingFinal.classList.remove('checked')
+//         fourStarRatingFinal.classList.remove('checked')
+//         fiveStarRatingFinal.classList.remove('checked')
+//       }
+//       if (rating = 3) {
+//         oneStarRatingFinal.classList.add('checked')
+//         twoStarRatingFinal.classList.add('checked')
+//         threeStarRatingFinal.classList.add('checked')
+//         fourStarRatingFinal.classList.remove('checked')
+//         fiveStarRatingFinal.classList.remove('checked')
+//       }
+//       if (rating = 4) {
+//         oneStarRatingFinal.classList.add('checked')
+//         twoStarRatingFinal.classList.add('checked')
+//         threeStarRatingFinal.classList.add('checked')
+//         fourStarRatingFinal.classList.add('checked')
+//         fiveStarRatingFinal.classList.remove('checked')
+//       }
+//       if (rating = 5) {
+//         oneStarRatingFinal.classList.add('checked')
+//         twoStarRatingFinal.classList.add('checked')
+//         threeStarRatingFinal.classList.add('checked')
+//         fourStarRatingFinal.classList.add('checked')
+//         fiveStarRatingFinal.classList.add('checked')
+//       }
+// }
+
+// final()
--- public/js/review.js
+++ public/js/review.js
@@ -42,10 +42,6 @@ document
   .addEventListener("submit", reviewHandler);
   
 
-
-
-
-
 //Code snippet for the navbar
 const list = document.querySelectorAll('.list');
 function activeLink() {
@@ -57,9 +53,73 @@ list.forEach((item) =>
   item.addEventListener("click", activeLink))
 
 
+
+
+//Code Snippet for the background
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 80, density: { enable: true, value_area: 800 } },
+      color: { value: "#ffffff" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#000000" },
+        polygon: { nb_sides: 5 },
+        image: { src: "img/github.svg", width: 100, height: 140 }
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "repulse" },
+        resize: true
+      },
+      modes: {
+        grab: { distance: 400, line_linked: { opacity: 1 } },
+        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+        repulse: { distance: 200, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 }
+      }
+    },
+    retina_detect: true
+  });
+  
+
+
+
+
 //Five Star Rating System
 //Couldnt get this figured out in time
-// let totalrating;
+
+// let totalrating = 0;
 
 // const oneStarRating = document.getElementById('1star')
 // const twoStarRating = document.getElementById('2star')
@@ -67,25 +127,11 @@ list.forEach((item) =>
 // const fourStarRating = document.getElementById('4star')
 // const fiveStarRating = document.getElementById('5star')
 
-// if (oneStarRating) {
-//   oneStarRating.addEventListener("click", rating1)
-// }
-// if (twoStarRating) {
-//   twoStarRating.addEventListener("click", rating2)
-// }
-// if (threeStarRating) {
-//   threeStarRating.addEventListener("click", rating3)
-// }
-// if (fourStarRating) {
-//   fourStarRating.addEventListener("click", rating4)
-// }
-// if (fiveStarRating) {
-//   fiveStarRating.addEventListener("click", rating5)
-// }
+
 
 // function rating1() {
 //   totalrating = 1
-//   final()
+//   // final()
 //   console.log(totalrating)
 //   oneStarRating.classList.add('checked')
 //   twoStarRating.classList.remove('checked')
@@ -96,7 +142,7 @@ list.forEach((item) =>
 
 // function rating2() {
 //   totalrating = 2
-//   final()
+//   // final()
 //   console.log(totalrating)
 //   oneStarRating.classList.add('checked')
 //   twoStarRating.classList.add('checked')
@@ -107,7 +153,7 @@ list.forEach((item) =>
 
 // function rating3() {
 //   totalrating = 3
-//   final()
+//   // final()
 //   console.log(totalrating)
 //   oneStarRating.classList.add('checked')
 //   twoStarRating.classList.add('checked')
@@ -118,7 +164,7 @@ list.forEach((item) =>
 
 // function rating4() {
 //   totalrating = 4
-//   final()
+//   // final()
 //   console.log(totalrating)
 //   oneStarRating.classList.add('checked')
 //   twoStarRating.classList.add('checked')
@@ -129,7 +175,7 @@ list.forEach((item) =>
 
 // function rating5() {
 //   totalrating = 5
-//   final()
+//   // final()
 //   console.log(totalrating)
 //   oneStarRating.classList.add('checked')
 //   twoStarRating.classList.add('checked')
@@ -138,46 +184,5 @@ list.forEach((item) =>
 //   fiveStarRating.classList.add('checked')
 // }
 
-// const oneStarRatingFinal = document.getElementById('1starFinal')
-// const twoStarRatingFinal = document.getElementById('2starFinal')
-// const threeStarRatingFinal = document.getElementById('3starFinal')
-// const fourStarRatingFinal = document.getElementById('4starFinal')
-// const fiveStarRatingFinal = document.getElementById('5starFinal')
-    
-// function final() {
-//       if (totalrating = 1) {
-//         oneStarRatingFinal.classList.add('checked')
-//         twoStarRatingFinal.classList.remove('checked')
-//         threeStarRatingFinal.classList.remove('checked')
-//         fourStarRatingFinal.classList.remove('checked')
-//         fiveStarRatingFinal.classList.remove('checked')
-//       }
-//       if (totalrating = 2) {
-//         oneStarRatingFinal.classList.add('checked')
-//         twoStarRatingFinal.classList.add('checked')
-//         threeStarRatingFinal.classList.remove('checked')
-//         fourStarRatingFinal.classList.remove('checked')
-//         fiveStarRatingFinal.classList.remove('checked')
-//       }
-//       if (totalrating = 3) {
-//         oneStarRatingFinal.classList.add('checked')
-//         twoStarRatingFinal.classList.add('checked')
-//         threeStarRatingFinal.classList.add('checked')
-//         fourStarRatingFinal.classList.remove('checked')
-//         fiveStarRatingFinal.classList.remove('checked')
-//       }
-//       if (totalrating = 4) {
-//         oneStarRatingFinal.classList.add('checked')
-//         twoStarRatingFinal.classList.add('checked')
-//         threeStarRatingFinal.classList.add('checked')
-//         fourStarRatingFinal.classList.add('checked')
-//         fiveStarRatingFinal.classList.remove('checked')
-//       }
-//       if (totalrating = 5) {
-//         oneStarRatingFinal.classList.add('checked')
-//         twoStarRatingFinal.classList.add('checked')
-//         threeStarRatingFinal.classList.add('checked')
-//         fourStarRatingFinal.classList.add('checked')
-//         fiveStarRatingFinal.classList.add('checked')
-//       }
-// }
+// export  {totalrating};
+
